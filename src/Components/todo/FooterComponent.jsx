9f1c12a34ef36a60f33f4da5684ddaff35adb3a1
abhyusthsahu <abhyusthsahu@gmail.com>
2026-06-21 19:40:25 +0530

    Sharing state with multiple components using Auth Context

--- src/Components/todo/FooterComponent.jsx
+++ src/Components/todo/FooterComponent.jsx
@@ -1,4 +1,11 @@
+import { useContext } from 'react';
+import { AuthContext } from './security/AuthContext';
+
 function FooterComponent() {
+
+    const authContext = useContext(AuthContext);
+    console.log(`FooterComponent: ${authContext.number}`);
+
     return (
         <footer className="FooterComponent">
             <div className="container">
